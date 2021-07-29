2d5a4b3af9c8b4332798a491bc6bd4f1d2682b87
Sasinda98 <sasindarupasighe@icloud.com>
2021-07-29 10:00:47 +0530

    Initial commit.

---
+++ src/test/java/com/gsr/petclinicsfg/petclinicsfg/PetClinicSfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsr.petclinicsfg.petclinicsfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
